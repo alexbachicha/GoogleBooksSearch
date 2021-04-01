57a1b9f2aced5fe95169732f44789f2624affd8b
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-03-31 19:02:33 -0700

    defined routes

--- googlebooksearch/src/App.js
+++ googlebooksearch/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+      <div>
+        
+      </div>
+    </Router>
   );
 }
 
