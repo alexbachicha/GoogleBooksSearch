fc061e3be3a89b23c7d31ce484df10022e4f2679
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-04-03 11:53:01 -0700

    changed placeholder phrase in book search, jumbotron and nav

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -12,7 +12,7 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
           id="Title"
           type="text"
           value={q}
-          placeholder="Ready Player One"
+          placeholder="What do you want to read today?"
           name="q"
           onChange={handleInputChange}
           required
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -37,9 +37,9 @@ class Saved extends Component {
           <Col size="md-12">
             <Jumbotron>
               <h1 className="text-center">
-                <strong>(React) Google Books Search</strong>
+                <strong>React Google Books Search</strong>
               </h1>
-              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+              <h2 className="text-center">Search and Save Your Favorite Books!</h2>
             </Jumbotron>
           </Col>
         </Row>
